5a77738c9a5f826adbcca80166e9c902e71a3241
Andrés <andcarv89@gmail.com>
2020-10-21 10:57:10 -0300

    cepas challenge

---
+++ app/models/wine.rb
@@ -0,0 +1,13 @@
+class Wine < ApplicationRecord
+    has_many :wine_strains
+    has_many :strains, through: :wine_strains, dependent: :destroy
+
+    def strain_percentage
+        @strain_percentage = []
+        self.wine_strains.each do |wstrain| 
+            @strain_percentage.push("#{wstrain.strain.name} " + "#{wstrain.percentage.to_s} %") 
+        end
+        @strain_percentage.join(', ')
+        
+    end 
+end
---
+++ db/migrate/20201020001413_add_percentage_to_wine_strain.rb
@@ -0,0 +1,5 @@
+class AddPercentageToWineStrain < ActiveRecord::Migration[5.2]
+  def change
+    add_column :wine_strains, :percentage, :float
+  end
+end
