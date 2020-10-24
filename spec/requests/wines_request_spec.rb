f7b01dbc7a4447298d93951285bc42e2c0b5ca8c
Andrés <andcarv89@gmail.com>
2020-10-24 10:55:03 -0300

    fixing test

--- spec/requests/wines_request_spec.rb
+++ spec/requests/wines_request_spec.rb
@@ -3,17 +3,26 @@ require 'rails_helper'
 RSpec.describe "Wines", type: :request do
 
     describe 'GET index ' do
-        context "with valid params" do
+        context "Singed in" do
             
             before do
                 user = User.create(email: "test@test.com")
                 login_as(user, :scope => :user)
             end
 
+            it 'Check wines create and show in index' do
+                wine = Wine.create(name: 'Wine name')
+                wines = Wine.all
+                get "/wines/"
+                expect(assigns(:wines)).to eq(wines)
+            end
 
     
     
             it 'renders the index template' do
+                get "/wines/"
+                expect(response).to render_template('index')
+            end
                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                   
 
 
@@ -22,11 +31,20 @@ RSpec.describe "Wines", type: :request do
     end
 
     describe 'GET show' do
-        context "with valid params" do
+        
+        context "Singed in" do
             
             before do
+                
                 user = User.create(email: "test@test.com")
                 login_as(user, :scope => :user)
+            end
+
+            it 'renders the show template' do
+                wine = Wine.create(name: "Test")
+                id = wine.id
+                get "/wines/#{id}/"
+                expect(response).to render_template('wines/show')
             end
 
 
